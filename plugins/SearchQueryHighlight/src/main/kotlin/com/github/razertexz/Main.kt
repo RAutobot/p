66f3fad5d085491de0301bbde0fab4f6caab4f7a
RAutobot <245641318+RAutobot@users.noreply.github.com>
2026-02-08 12:27:24 +0700

    misc: update

--- plugins/SearchQueryHighlight/src/main/kotlin/com/github/razertexz/Main.kt
+++ plugins/SearchQueryHighlight/src/main/kotlin/com/github/razertexz/Main.kt
@@ -20,10 +20,13 @@ internal class Main : Plugin() {
         patcher.patch(WidgetChatListAdapterItemMessage::class.java, "processMessageText", arrayOf(SimpleDraweeSpanTextView::class.java, MessageEntry::class.java), object : XC_MethodHook() {
             override fun afterHookedMethod(param: XC_MethodHook.MethodHookParam) {
                 val data = (param.thisObject as WidgetChatListAdapterItemMessage).adapter.data
-                if (data !is WidgetSearchResults.Model) return
+                if (data is WidgetSearchResults.Model) {
+                    val textView = param.args[0] as SimpleDraweeSpanTextView
+                    val spannable = textView.text as Spannable
+                    spannable.setSpan()
 
-                val textView = param.args[0] as SimpleDraweeSpanTextView
-                val spannable = textView.getText() as Spannable
+                    textView.setText(spannable)
+                }
             }
         })
     }
